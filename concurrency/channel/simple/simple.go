caecc8f16c2fd13008a30019f135f67658ba1d85
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-08-10 10:38:02 -0400

    update-automated-save

--- concurrency/channel/simple/simple.go
+++ concurrency/channel/simple/simple.go
@@ -3,11 +3,12 @@ package simple
 func NewChanOwner(num int) func() <-chan int {
 	ch := make(chan int, num)
 	chanOwner := func() <-chan int {
+
 		go func() {
+			defer close(ch)
 			for i := 0; i < num; i++ {
 				ch <- i
 			}
-			close(ch)
 		}()
 		return ch
 	}
